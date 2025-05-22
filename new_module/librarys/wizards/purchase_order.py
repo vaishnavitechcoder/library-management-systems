90f112a44e14f227afbf01876be0603609bed1f9
vaishnavitechcoder <jobsforvaishnavipatil@gmail.com>
2025-05-22 14:25:27 +0530

    change button name on purchase order xml file

--- new_module/librarys/wizards/purchase_order.py
+++ new_module/librarys/wizards/purchase_order.py
@@ -9,7 +9,7 @@ class PurchaseOrderWizard(models.TransientModel):
     _name = 'purchase.order.wizard'
     _description = 'Purchase Wizard to export selected employees to Excel'
 
-    vendor = fields.Many2many('res.partner', string="Vendors")
+    vendors = fields.Many2many('res.partner', string="Vendors")
     file_name = fields.Char(string="File Name", default="employee_export.xlsx")
     file_data = fields.Binary(string="Download Excel", readonly=True)
 
@@ -22,7 +22,7 @@ class PurchaseOrderWizard(models.TransientModel):
         if not selected_vendors:
             raise UserError("Selected vendors do not exist.")
 
-        vendors = self.env['purchase.order'].search([('partner_id', 'in', self.vendor.ids)])
+        vendors = self.env['purchase.order'].search([('partner_id', 'in', selected_vendors.ids)])
         if not vendors:
             raise UserError("No Purchase Orders found for selected vendors.")
         # vendors = self.env['purchase.order'].browse(active_ids)
